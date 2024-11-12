d21218cfdcf3f04de7d8361f172b418c3bd9e4e1
kiw1meister <linandy719@gmail.com>
2024-11-12 15:17:54 +0900

    feat: add board

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        int tileSize = 32; //tile size in pixels
+        int rows = 16; //board rows
+        int columns = 16; //board columns
+        int boardWidth = tileSize * columns; //total width of game board
+        int boardHeight = tileSize * rows; //total height of game board
+
+        JFrame frame = new JFrame("Spice Inviters");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
